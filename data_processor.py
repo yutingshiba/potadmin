a738356c16b56c5396f43a64815571f383cad01a
YuTing <peter1234603@gmail.com>
2019-01-17 07:50:27 +0000

    model, data_processor

--- data_processor.py
+++ data_processor.py
@@ -45,26 +45,39 @@ def parse_posts(posts_str, trunc_size=100, no_stopwords=False):
     posts = posts_str.split('|||')
     # filter out invalid posts
     posts_list = []
+    url_count_list = []
     for post in posts:
         # replace url
         _post = re.sub(URL_RE, '<URL>', post)
 
         # to lower cast  \\  Drop if not alpha
-        words = [w.lower() for w in _post.split() if w.isalpha()]
+        words = [w.lower() for w in _post.split()]
+        #print(words)
+        url_count = 0
+        for w in words:
+            if w  == '<url>':
+                url_count += 1
+        alpha_words = [w for w in words if w.isalpha()]
+        #print(alpha_words, url_count)
         
         # Remove punctuation
 
         punc_table = str.maketrans('', '', string.punctuation)
-        stripped = [w.translate(punc_table) if w != '<url>' else w for w in words]
+        stripped = [w.translate(punc_table) if w != '<url>' else w for w in alpha_words]
         if no_stopwords:
             posts_list.append(stripped[:trunc_size])
             continue
+        #print(stripped)
 
         # Remove stopwords
         stop_words = stopwords.words('english')
         clean_words = [w for w in stripped if w and w not in stop_words]
+        #print(clean_words)
 
-        posts_list.append(clean_words[:trunc_size])
+        #input()
+        if clean_words:
+            posts_list.append(clean_words[:trunc_size])
+            url_count_list.append(url_count)
         
         '''
         # Stemming
@@ -72,7 +85,34 @@ def parse_posts(posts_str, trunc_size=100, no_stopwords=False):
         stemmed = [porter.stem(w) for w in clean_words]
         posts_list.append(stemmed[:trunc_size])
         '''
-    return posts_list
+    return posts_list, url_count_list
+
+
+def gen_all_csv(fname='_data/complete.csv'):
+    fp = open(fname, 'r')
+    eifp = open('_data/EI_all_url)unsh.csv', 'w')
+    nsfp = open('_data/NS_all_url_unsh.csv', 'w')
+    tffp = open('_data/TF_all_url_unsh.csv', 'w')
+    jpfp = open('_data/JP_all_url_unsh.csv', 'w')
+    fp.readline()
+    for line in fp:
+        labels = line[:4]
+        posts = line.rstrip('\n')[5:]
+        posts_list, urlc_list = parse_posts(posts)
+        for idx, post in enumerate(posts_list):
+            # E I
+            label = 1 if labels[0] == 'E' else 0
+            eifp.write('{}\t{}\t{}\n'.format(label, post, urlc_list[idx]))
+            # N S
+            label = 1 if labels[1] == 'N' else 0
+            nsfp.write('{}\t{}\t{}\n'.format(label, post, urlc_list[idx]))
+            # T F
+            label = 1 if labels[2] == 'T' else 0
+            tffp.write('{}\t{}\t{}\n'.format(label, post, urlc_list[idx]))
+            # J P
+            label = 1 if labels[3] == 'J' else 0
+            jpfp.write('{}\t{}\t{}\n'.format(label, post, urlc_list[idx]))
+
 
 
 def padded(post, word_vec, unk,  pad_len=64):
@@ -88,9 +128,10 @@ def padded(post, word_vec, unk,  pad_len=64):
     
     return post_emb
 
-def generate_from_file(path, batch_size, word_vec, is_test=False):
+def generate_from_file(path, batch_size, word_vec, with_url=False, is_test=False):
     label_list = []
     post_list = []
+    url_list = []
     # Load data
     min_len = 4 
     max_len = 40 
@@ -102,20 +143,29 @@ def generate_from_file(path, batch_size, word_vec, is_test=False):
                 continue
             label_list.append(int(tokens[0]))
             post_list.append(post[:max_len])
+            if with_url:
+                url_list.append(int(tokens[2]))
 
     # Shuffle data
-    label_list, post_list = shuffle(label_list, post_list)
+    if with_url:
+        label_list, post_list, url_list = shuffle(label_list, post_list, url_list)
+    else:
+        label_list, post_list = shuffle(label_list, post_list)
 
     # Generate batches
     nb_batch = math.ceil(len(label_list) / batch_size)
-    #word_unk = [random.uniform(-1, 1) for n in range(300)]
-    word_unk = [-1 for n in range(300)]
+    word_unk = [random.uniform(-1, 1) for n in range(300)]
+    #word_unk = [-1 for n in range(300)]
     
     while 1:
         for n in range(nb_batch):
             inputs = []
-            for post in post_list[n*batch_size: (n+1)*batch_size]:
-                inputs.append(padded(post, word_vec, word_unk, pad_len=max_len))
+            if with_url:
+                for idx, post in enumerate(post_list[n*batch_size: (n+1)*batch_size]):
+                    inputs.append([padded(post, word_vec, word_unk, pad_len=max_len), url_list[n*batch_size + idx]])
+            else:
+                for post in post_list[n*batch_size: (n+1)*batch_size]:
+                    inputs.append(padded(post, word_vec, word_unk, pad_len=max_len))
             outputs = label_list[n*batch_size: (n+1)*batch_size]
             
             if is_test:
@@ -168,5 +218,6 @@ def load_test_true(path):
     return np.array(label_list)
 
 if __name__ == '__main__':
-    ll = load_test_true('_data/TF_test.csv')
-    print(ll.shape[0])
+    gen_all_csv()
+    #ll = load_test_true('_data/TF_test.csv')
+    #print(ll.shape[0])
